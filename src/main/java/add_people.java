2cf313d307159248d9d218ed60ae2919e9af107b
kamil <tyrekkamil@gmail.com>
2018-10-30 13:59:23 +0100

    tl

--- src/main/java/add_people.java
+++ src/main/java/add_people.java
@@ -1,6 +1,81 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class add_people {
+
     public static void main(String[] args)
+
     {
-        System.out.println("Test\ntest");
+        Scanner reader = new Scanner(System.in);  // Reading from System.in
+        HashMap people;
+        Array list = new ArrayList();
+
+        while(true)
+        {
+            people = new HashMap();
+            System.out.println("Wpisz miasto:");
+            String city = reader.nextLine();
+            String other = reader.nextLine();
+            String[] sp = other.split(" ");
+
+            if(sp[0] != null && sp[1] != null && sp[2]!= null)
+            {
+                String pesel = sp[2];
+                System.out.println(isPeselGood(pesel));
+
+            }
+
+            else
+            {
+                System.out.println("Nie podano wszystkich danych. Wprowadz ponownie miasto, a następnie imię, nazwisko, pesel");
+            }
+
+
+        }
+
+
+
+
     }
+
+    public static Boolean isPeselGood(String p)
+    {
+        if(p.length()==11 && checkPesel(p)==true) return true;
+        else
+        {
+             return false;
+        }
+    }
+
+    public static Boolean checkPesel(String p)
+    {
+    long psl = Long.parseLong(p);
+    long psl_id[] = new long[11];
+    int i=9;
+    psl_id[10]=psl%10;
+    psl=psl/10;
+
+    while(i>=0)
+    {
+    psl_id[i]=psl%10;
+    psl=psl/10;
+    i--;
+    }
+
+    float sum=psl_id[0]+3*psl_id[1]+7*psl_id[2]+9*psl_id[3]+psl_id[4]+3*psl_id[5]+7*psl_id[6]+9*psl_id[7]+psl_id[8]+3*psl_id[9];
+    sum %= 10;
+    sum = 10 - sum;
+    sum %= 10;
+    System.out.println(sum);
+    System.out.println(psl_id[10]);
+
+    if(sum==psl_id[10])
+        return true;
+    else
+        return false;
+    }
+
+
 }
